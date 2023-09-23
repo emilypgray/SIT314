cb805b0c7a580f8a5aaba4da5259335390f06280
Emily Gray <87569476+emilypgray@users.noreply.github.com>
2023-09-23 16:28:19 +1000

    Update script.js

--- 7.1 code/Application Code/public/script.js
+++ 7.1 code/Application Code/public/script.js
@@ -1,89 +1,108 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('mqttCredentialsModal');
+    const submitBtn = document.getElementById('submitCredentialsBtn');
+    const usernameField = document.getElementById('mqttUsername');
+    const passwordField = document.getElementById('mqttPassword');
+    const closeModalBtn = document.querySelector('.close');
 
-  const modal = document.getElementById('mqttCredentialsModal');
-  const submitBtn = document.getElementById('submitCredentialsBtn');
-  const usernameField = document.getElementById('mqttUsername');
-  const passwordField = document.getElementById('mqttPassword');
-  const closeModalBtn = document.querySelector('.close');
+    // Show the modal upon page loading
+    modal.style.display = 'block';
 
-  function submitCredentials() {
-      const mqttCredentials = {
-          username: usernameField.value,
-          password: passwordField.value
-      };
+    // Close the modal when the 'x' button is clicked
+    closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
 
-      fetch('/set-mqtt-credentials', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(mqttCredentials)
-      }).then(response => {
-          if (response.status !== 200) {
-              console.error('Failed to set MQTT credentials on server');
-          } else {
-              // Close the modal if the credentials are successfully sent
-              modal.style.display = 'none';
-          }
-      });
-  }
+    function submitCredentials() {
+        const credentials = {
+            username: usernameField.value,
+            password: passwordField.value
+        };
+        // post credentials to server
+        fetch('/set-credentials', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(response => {
+            if (response.status !== 200) {
+                return response.json().then(err => {
+                    throw new Error(err.message || 'Login failed');
+                });
+            }
+            modal.style.display = 'none';
+            setTimeout(establishEventSource, 2000);
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+            alert(error.message);
+        });
+    }
+    submitBtn.addEventListener('click', submitCredentials);
 
-  // Attach the submit function to the button
-  submitBtn.addEventListener('click', submitCredentials);
 
-  const hallwayLED = document.getElementById('hallwayLED');
-  const frontroomLED = document.getElementById('frontroomLED');
-  const eventSource = new EventSource('/events');
+    function establishEventSource() {
+        const hallwayLED = document.getElementById('hallwayLED');
+        const frontroomLED = document.getElementById('frontroomLED');
+        const eventSource = new EventSource('/events');
 
-  eventSource.onmessage = (event) => {
-      const { topic, message } = JSON.parse(event.data);
-      console.log(`Received message on ${topic}: ${message}`);
+        // when a message is received from the broker, check the state of the switches
+        // to see if they match the state of the light
+        eventSource.onmessage = (event) => {
+            const { topic, message } = JSON.parse(event.data);
 
-      if (topic === '/sensors/leds/hallway/state') {
-          hallwayLED.checked = message === '1';
-      } else if (topic === '/sensors/leds/frontroom/state') {
-          frontroomLED.checked = message === '1';
-      }
-  };
+            if (topic === '/sensors/leds/hallway/state') {
+                hallwayLED.checked = message === '1';
+            } else if (topic === '/sensors/leds/frontroom/state') {
+                frontroomLED.checked = message === '1';
+            }
+        };
 
-  eventSource.onerror = (error) => {
-      console.error('EventSource failed:', error);
-      eventSource.close();
-  };
+        eventSource.onerror = (error) => {
+            console.error('EventSource failed:', error);
+            eventSource.close();
+        };
 
-  hallwayLED.addEventListener('change', () => {
-      const message = hallwayLED.checked ? 'on' : 'off';
-      fetch('/process-form', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              lightId: 'hallwayLED',
-              lightState: message
-          })
-      });
-  });
+        // add event listeners for the toggle switches
+        hallwayLED.addEventListener('change', toggleLightState);
+        frontroomLED.addEventListener('change', toggleLightState);
+    }
 
-  frontroomLED.addEventListener('change', () => {
-      const message = frontroomLED.checked ? 'on' : 'off';
-      fetch('/process-form', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              lightId: 'frontroomLED',
-              lightState: message
-          })
-      });
-  });
+    function toggleLightState(event) {
 
-  // Show the modal upon page loading
-  modal.style.display = 'block';
+        const light = event.target;
+        const message = light.checked ? 'on' : 'off';
+        const lightId = light.id;
 
-  // Close the modal when the 'x' button is clicked
-  closeModalBtn.addEventListener('click', () => {
-      modal.style.display = 'none';
-  });
+        // post the state of the toggle switch to the server when a change happens
+        fetch('/process-form', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                lightId,
+                lightState: message
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(text => {
+                    throw new Error(text || 'Server Error');
+                });
+            }
+            // Check if the response is JSON before parsing
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+                return response.json();
+            } else {
+                return response.text();
+            }
+        })
+        .catch(error => {
+            console.error('Failed to toggle light state:', error);
+        });
+    }
 });
