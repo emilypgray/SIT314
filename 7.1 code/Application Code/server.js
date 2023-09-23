bf983249f17ddd749508497ce1dc84b71fb2c27b
Emily Gray <87569476+emilypgray@users.noreply.github.com>
2023-09-23 11:57:21 +1000

    Update server.js

--- 7.1 code/Application Code/server.js
+++ 7.1 code/Application Code/server.js
@@ -1,10 +1,8 @@
-// import required modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const mqtt = require('mqtt');
-const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const port = 3000;
 const IPv4_HOST = '0.0.0.0';
@@ -13,61 +11,64 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let mqttCredentials = {};
 let client;
+let storedCredentials = JSON.parse(fs.readFileSync('credentials.json', 'utf-8'));
 
-app.post('/set-mqtt-credentials', (req, res) => {
-    mqttCredentials = req.body;
+app.post('/set-credentials', (req, res) => {
+    const { username, password } = req.body;
 
-    // Disconnect from previous MQTT connection if it exists
-    if (client && client.connected) {
-        client.end();
-    }
+    if (username === storedCredentials.username && password === storedCredentials.password) {
+        console.log('Login Successful');
+        // Disconnect from previous MQTT connection if it exists
+        if (client && client.connected) {
+            client.end();
+        }
 
-    // Connect to MQTT with new credentials
-    const mqttBrokerOptions = {
-        hostname: '172.31.11.33',
-        port: 1883,
-        clientId: 'server_client',
-        username: mqttCredentials.username,
-        password: mqttCredentials.password,
-        protocol: 'mqtt',
-    };
+        // Connect to MQTT
+        const mqttBrokerOptions = {
+            hostname: 'a3kvpx8bd0q9lp-ats.iot.ap-southeast-2.amazonaws.com',
+            port: 8883,
+            clientId: 'server_client',
+            protocol: 'mqtts',
+            key: fs.readFileSync('certs/private.pem.key'),
+            cert: fs.readFileSync('certs/device.pem.crt'),
+            ca: fs.readFileSync('certs/Amazon-root-CA-1.pem'),
+        };
 
-    client = mqtt.connect(mqttBrokerOptions);
+        client = mqtt.connect(mqttBrokerOptions);
 
-    // MQTT Event Handlers
-    client.on('connect', () => {
-        console.log('Connected to MQTT broker');
-
-        // Subscribe to the topics
-        client.subscribe('/sensors/leds/hallway/state');
-        client.subscribe('/sensors/leds/frontroom/state');
-    });
-
-    client.on('message', (topic, message) => {
-        // Send data to all connected clients using SSE
-        clients.forEach(clientRes => {
-            clientRes.write(`data: ${JSON.stringify({ topic, message: message.toString() })}\n\n`);
+        client.on('connect', () => {
+            console.log('Connected to MQTT broker');
+            client.subscribe('/sensors/leds/hallway/state');
+            client.subscribe('/sensors/leds/frontroom/state');
         });
-    });
 
-    res.sendStatus(200);
+        client.on('message', (topic, message) => {
+            clients.forEach(clientRes => {
+                clientRes.write(`data: ${JSON.stringify({ topic, message: message.toString() })}\n\n`);
+            });
+        });
+
+        res.sendStatus(200);
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
 });
 
-// Store clients so that messages can be broadcasted to all connected clients
 const clients = [];
 
 app.get('/events', (req, res) => {
+    if (!client || !client.connected) {
+        res.status(401).send('Unauthorized: Please provide valid credentials first.');
+        return;
+    }
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
     res.flushHeaders();
 
-    // Add this client to the clients list
     clients.push(res);
 
-    // When client closes connection we update the clients list
     req.on('close', () => {
         const index = clients.indexOf(res);
         if (index !== -1) {
@@ -76,14 +77,16 @@ app.get('/events', (req, res) => {
     });
 });
 
-// published new state of light (from web app) to the broker
 app.post('/process-form', (req, res) => {
+    if (!client || !client.connected) {
+        res.status(401).send('Unauthorized: Please provide valid credentials first.');
+        return;
+    }
     const formData = req.body;
     const topic = formData.lightId === 'hallwayLED' ? '/sensors/leds/hallway/command' : '/sensors/leds/frontroom/command';
     const light = formData.lightState === 'on' ? '1' : '0';
-    console.log(`Publishing to ${topic}: ${light}`);
-    client.publish(topic, light, { qos: 1 });
 
+    client.publish(topic, light, { qos: 1 });
     res.send('Form data received and message sent to MQTT broker.');
 });
 
